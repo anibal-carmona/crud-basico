4b3e8e4a973c2e0adb5dd4a74fa721086c334b9b
anibal-carmona <anibal.carmona@outlook.com>
2021-11-04 00:27:43 -0300

    Se modifica login con template

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,25 +1,116 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-@section('content')
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-<form action="{{ route('login') }}" method="POST">
+    <title>Login</title>
 
-    @csrf
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="{{asset('assets/images/favicon.ico')}}">
 
-    <input type="email" name="email" value="{{ old('email')}}" placeholder="Correo Electrónico...">
-    <br>
-    @error('email')
-        <span>{{ $message }}</span>
-    @enderror
-    <br>
-    <input type="password" name="password" placeholder="Contraseña...">
-    <br>
-    @error('password')
-        <span>{{ $message }}</span>
-    @enderror
-    <br>
-    <button type="submit">Login</button>
+    <!-- App css -->
+    <link href="{{asset('assets/css/icons.min.css')}}" rel="stylesheet" type="text/css" />
+    <link href="{{asset('assets/css/app-creative.min.css')}}" rel="stylesheet" type="text/css" id="light-style" />
+    <link href="{{asset('assets/css/app-creative-dark.min.css')}}" rel="stylesheet" type="text/css" id="dark-style" />
 
-</form>
+    @livewireStyles
 
-@endsection
+</head>
+
+<body class="authentication-bg" data-layout-config='{"darkMode":false}'>
+
+    <div class="account-pages pt-2 pt-sm-5 pb-4 pb-sm-5">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-xxl-4 col-lg-5">
+                    <div class="card">
+
+                        <!-- Logo -->
+                        {{-- <div class="card-header pt-4 pb-4 text-center bg-primary">
+                            <a href="index.html">
+                                <span><img src="assets/images/logo.png" alt="" height="18"></span>
+                            </a>
+                        </div> --}}
+
+                        <div class="card-body p-4">
+                            
+                            <div class="text-center w-75 m-auto">
+                                <h4 class="text-dark-50 text-center mt-0 fw-bold">Log In</h4>
+                                <p class="text-muted mb-4">Ingrese su email y contraseña.</p>
+                            </div>
+
+                            <form action="{{ route('login') }}" method="POST">
+
+                                @csrf
+
+                                <div class="mb-3">
+                                    <label class="form-label" for="email">Email</label>
+                                    <input class="form-control" type="email" name="email" value="{{ old('email')}}" required="" placeholder="Correo electrónico...">
+                                    
+                                    @error('email')                          
+                                        <span class="help-block" style="color: #fa6767"><small>{{ $message }}</small></span>
+                                    @enderror          
+                                    
+                                </div>
+
+                                <div class="mb-3">
+                                    {{-- <a href="pages-recoverpw.html" class="text-muted float-end"><small>Forgot your password?</small></a> --}}
+                                    <label for="password" class="form-label">Contraseña</label>
+                                    <div class="input-group input-group-merge">
+                                        <input type="password" name="password" class="form-control" placeholder="Contraseña...">
+                                        <div class="input-group-text" data-password="false">
+                                            <span class="password-eye"></span>
+                                        </div>
+                                    </div>
+
+                                    @error('password')
+                                        <span class="help-block" style="color: #fa6767"><small>{{ $message }}</small></span>
+                                    @enderror   
+                                </div>
+
+                                <div class="mb-4 mb-3">
+                                    {{-- <div class="form-check">
+                                        <input type="checkbox" class="form-check-input" id="checkbox-signin" checked>
+                                        <label class="form-check-label" for="checkbox-signin">Remember me</label>
+                                    </div> --}}
+                                </div>
+
+                                <div class="mb-3 mb-0 text-center">
+                                    <button class="btn btn-primary" type="submit"> Entrar </button>
+                                </div>
+
+                            </form>
+                        </div> <!-- end card-body -->
+                    </div>
+                    <!-- end card -->
+
+                    {{-- <div class="row mt-3">
+                        <div class="col-12 text-center">
+                            <p class="text-muted">Don't have an account? <a href="pages-register.html" class="text-muted ms-1"><b>Sign Up</b></a></p>
+                        </div> <!-- end col -->
+                    </div> --}}
+                    <!-- end row -->
+
+                </div> <!-- end col -->
+            </div>
+            <!-- end row -->
+        </div>
+        <!-- end container -->
+    </div>
+    <!-- end page -->
+
+    <footer class="footer footer-alt">
+        {{ now()->toDateString() }}
+    </footer>
+
+<!-- bundle -->
+<script src="{{asset('assets/js/vendor.min.js')}}"></script>
+<script src="{{asset('assets/js/app.min.js')}}"></script>    
+
+@livewireScripts
+    
+</body>
+</html>
